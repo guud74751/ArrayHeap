8dfdb8d49bdf46a710a51d6e1d5634323967068a
guud <guud74751@gmail.com>
2023-04-02 23:33:57 +0800

    First commit

---
+++ ArrayHeap.java
@@ -0,0 +1,358 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayHeap<T> {
+    private Node[] contents;
+    private int size;
+
+    public ArrayHeap() {
+        contents = new ArrayHeap.Node[16];
+        contents[0] = null;
+        size = 0;
+    }
+
+    private static int leftIndex(int i) {
+        return 2 * i;
+    }
+
+    private static int rightIndex(int i) {
+        return 2 * i + 1;
+    }
+
+    private static int parentIndex(int i) {
+        return i / 2;
+    }
+    private Node getNode(int index) {
+        if (!inBounds(index)) {
+            return null;
+        }
+        return contents[index];
+    }
+
+    private boolean inBounds(int index) {
+        return (index <= size) && (index >= 1);
+    }
+    private void swap(int index1, int index2) {
+        Node node1 = getNode(index1);
+        Node node2 = getNode(index2);
+        contents[index1] = node2;
+        contents[index2] = node1;
+    }
+
+    private int min(int index1, int index2) {
+        Node node1 = getNode(index1);
+        Node node2 = getNode(index2);
+        if (node1 == null) {
+            return index2;
+        } else if (node2 == null) {
+            return index1;
+        } else if (node1.myPriority < node2.myPriority) {
+            return index1;
+        } else {
+            return index2;
+        }
+    }
+
+    private void swim(int index) {
+        validateSinkSwimArg(index);
+
+        while (index != 1) {
+            Node now = getNode(index);
+            Node parent = getNode(parentIndex(index));
+            if (now.myPriority >= parent.myPriority) {
+                break;
+            }
+            swap(index, parentIndex(index));
+            index /= 2;
+        }
+    }
+
+    private void sink(int index) {
+        validateSinkSwimArg(index);
+
+        while (true) {
+            if (leftIndex(index) > size) {
+                break;
+            }
+            Node now = getNode(index);
+            int sonIndex = min(leftIndex(index), rightIndex(index));
+            Node son = getNode(sonIndex);
+            if (now.myPriority <= son.myPriority) {
+                break;
+            }
+            swap(index, sonIndex);
+            index = sonIndex;
+        }
+    }
+
+    public void insert(T item, double priority) {
+        if (size + 1 == contents.length) {
+            resize(contents.length * 2);
+        }
+
+        size += 1;
+        contents[size] = new Node(item, priority);
+        swim(size);
+    }
+
+    public T peek() {
+        return contents[1].myItem;
+    }
+
+    public T removeMin() {
+        swap(1, size);
+        T value = contents[size].myItem;
+        contents[size] = null;
+        size -= 1;
+        if (size != 0) {
+            sink(1);
+        }
+        return value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void changePriority(T item, double priority) {
+        int pos = 0;
+        for (int i = 1; i <= size; i += 1) {
+            if (contents[i].myItem.equals(item)) {
+                pos = i;
+                break;
+            }
+        }
+        swap(pos, size);
+        swim(pos);
+        sink(pos);
+        contents[size] = null;
+        size -= 1;
+        insert(item, priority);
+    }
+
+    public String toString() {
+        return toStringHelper(1, "");
+    }
+
+    private String toStringHelper(int index, String soFar) {
+        if (getNode(index) == null) {
+            return "";
+        } else {
+            String toReturn = "";
+            int rightChild = rightIndex(index);
+            toReturn += toStringHelper(rightChild, "        " + soFar);
+            if (getNode(rightChild) != null) {
+                toReturn += soFar + "    /";
+            }
+            toReturn += "\n" + soFar + getNode(index) + "\n";
+            int leftChild = leftIndex(index);
+            if (getNode(leftChild) != null) {
+                toReturn += soFar + "    \\";
+            }
+            toReturn += toStringHelper(leftChild, "        " + soFar);
+            return toReturn;
+        }
+    }
+
+    private void validateSinkSwimArg(int index) {
+        if (index < 1) {
+            throw new IllegalArgumentException("Cannot sink or swim nodes with index 0 or less");
+        }
+        if (index > size) {
+            throw new IllegalArgumentException("Cannot sink or swim nodes with index greater than current size.");
+        }
+        if (contents[index] == null) {
+            throw new IllegalArgumentException("Cannot sink or swim a null node.");
+        }
+    }
+
+    private class Node {
+        private T myItem;
+        private double myPriority;
+
+        private Node(T item, double priority) {
+            myItem = item;
+            myPriority = priority;
+        }
+
+        public T item(){
+            return myItem;
+        }
+
+        public double priority() {
+            return myPriority;
+        }
+
+        public String toString() {
+            return myItem.toString() + ", " + myPriority;
+        }
+    }
+
+
+    private void resize(int capacity) {
+        Node[] temp = new ArrayHeap.Node[capacity];
+        for (int i = 1; i < this.contents.length; i++) {
+            temp[i] = this.contents[i];
+        }
+        this.contents = temp;
+    }
+
+    @Test
+    public void testIndexing() {
+        assertEquals(6, leftIndex(3));
+        assertEquals(10, leftIndex(5));
+        assertEquals(7, rightIndex(3));
+        assertEquals(11, rightIndex(5));
+
+        assertEquals(3, parentIndex(6));
+        assertEquals(5, parentIndex(10));
+        assertEquals(3, parentIndex(7));
+        assertEquals(5, parentIndex(11));
+    }
+
+    @Test
+    public void testSwim() {
+        ArrayHeap<String> pq = new ArrayHeap<>();
+        pq.size = 7;
+        for (int i = 1; i <= 7; i += 1) {
+            pq.contents[i] = new ArrayHeap<String>.Node("x" + i, i);
+        }
+        // Change item x6's priority to a low value.
+
+        pq.contents[6].myPriority = 0;
+        System.out.println("PQ before swimming:");
+        System.out.println(pq);
+
+        // Swim x6 upwards. It should reach the root.
+
+        pq.swim(6);
+        System.out.println("PQ after swimming:");
+        System.out.println(pq);
+        assertEquals("x6", pq.contents[1].myItem);
+        assertEquals("x2", pq.contents[2].myItem);
+        assertEquals("x1", pq.contents[3].myItem);
+        assertEquals("x4", pq.contents[4].myItem);
+        assertEquals("x5", pq.contents[5].myItem);
+        assertEquals("x3", pq.contents[6].myItem);
+        assertEquals("x7", pq.contents[7].myItem);
+    }
+
+    @Test
+    public void testSink() {
+        ArrayHeap<String> pq = new ArrayHeap<>();
+        pq.size = 7;
+        for (int i = 1; i <= 7; i += 1) {
+            pq.contents[i] = new ArrayHeap<String>.Node("x" + i, i);
+        }
+        pq.contents[1].myPriority = 10;
+        System.out.println("PQ before sinking:");
+        System.out.println(pq);
+
+        pq.sink(1);
+        System.out.println("PQ after sinking:");
+        System.out.println(pq);
+        assertEquals("x2", pq.contents[1].myItem);
+        assertEquals("x4", pq.contents[2].myItem);
+        assertEquals("x3", pq.contents[3].myItem);
+        assertEquals("x1", pq.contents[4].myItem);
+        assertEquals("x5", pq.contents[5].myItem);
+        assertEquals("x6", pq.contents[6].myItem);
+        assertEquals("x7", pq.contents[7].myItem);
+    }
+
+
+    @Test
+    public void testInsert() {
+        ArrayHeap<String> pq = new ArrayHeap<>();
+        pq.insert("c", 3);
+        assertEquals("c", pq.contents[1].myItem);
+
+        pq.insert("i", 9);
+        assertEquals("i", pq.contents[2].myItem);
+
+        pq.insert("g", 7);
+        pq.insert("d", 4);
+        assertEquals("d", pq.contents[2].myItem);
+
+        pq.insert("a", 1);
+        assertEquals("a", pq.contents[1].myItem);
+
+        pq.insert("h", 8);
+        pq.insert("e", 5);
+        pq.insert("b", 2);
+        pq.insert("c", 3);
+        pq.insert("d", 4);
+        System.out.println("pq after inserting 10 items: ");
+        System.out.println(pq);
+        assertEquals(10, pq.size());
+        assertEquals("a", pq.contents[1].myItem);
+        assertEquals("b", pq.contents[2].myItem);
+        assertEquals("e", pq.contents[3].myItem);
+        assertEquals("c", pq.contents[4].myItem);
+        assertEquals("d", pq.contents[5].myItem);
+        assertEquals("h", pq.contents[6].myItem);
+        assertEquals("g", pq.contents[7].myItem);
+        assertEquals("i", pq.contents[8].myItem);
+        assertEquals("c", pq.contents[9].myItem);
+        assertEquals("d", pq.contents[10].myItem);
+    }
+
+
+    @Test
+    public void testInsertAndRemoveOnce() {
+        ArrayHeap<String> pq = new ArrayHeap<>();
+        pq.insert("c", 3);
+        pq.insert("i", 9);
+        pq.insert("g", 7);
+        pq.insert("d", 4);
+        pq.insert("a", 1);
+        pq.insert("h", 8);
+        pq.insert("e", 5);
+        pq.insert("b", 2);
+        pq.insert("c", 3);
+        pq.insert("d", 4);
+        String removed = pq.removeMin();
+        assertEquals("a", removed);
+        assertEquals(9, pq.size());
+        assertEquals("b", pq.contents[1].myItem);
+        assertEquals("c", pq.contents[2].myItem);
+        assertEquals("e", pq.contents[3].myItem);
+        assertEquals("c", pq.contents[4].myItem);
+        assertEquals("d", pq.contents[5].myItem);
+        assertEquals("h", pq.contents[6].myItem);
+        assertEquals("g", pq.contents[7].myItem);
+        assertEquals("i", pq.contents[8].myItem);
+        assertEquals("d", pq.contents[9].myItem);
+    }
+
+    @Test
+    public void testInsertAndRemoveAllButLast() {
+        ArrayHeap<String> pq = new ArrayHeap<>();
+        pq.insert("c", 3);
+        pq.insert("i", 9);
+        pq.insert("g", 7);
+        pq.insert("d", 4);
+        pq.insert("a", 1);
+        pq.insert("h", 8);
+        pq.insert("e", 5);
+        pq.insert("b", 2);
+        pq.insert("c", 3);
+        pq.insert("d", 4);
+
+        int i = 0;
+        String[] expected = {"a", "b", "c", "c", "d", "d", "e", "g", "h", "i"};
+        while (pq.size() > 1) {
+            assertEquals(expected[i], pq.removeMin());
+            i += 1;
+        }
+    }
+
+    @Test
+    public void test() {
+        ArrayHeap<Integer> pq = new ArrayHeap<>();
+        pq.insert(1, 1.0);
+        pq.insert(2, 2.0);
+        System.out.println(pq.removeMin());
+    }
+}
